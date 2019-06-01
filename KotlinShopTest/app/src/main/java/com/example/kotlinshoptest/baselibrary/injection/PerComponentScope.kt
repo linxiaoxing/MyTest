1abf7a5152322b5976f5739d929cc72649b1972a
林ショウコウ <rinsyouk525@gmail.com>
2019-06-01 16:56:15 +0900

    kotlin add file 2

---
+++ KotlinShopTest/app/src/main/java/com/example/kotlinshoptest/baselibrary/injection/PerComponentScope.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinshoptest.baselibrary.injection
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerComponentScope
---
+++ KotlinShopTest/app/src/main/java/com/example/kotlinshoptest/baselibrary/rx/BaseException.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinshoptest.baselibrary.rx
+
+class BaseException(val status: Int, val msg: String): Throwable() {
+
+}
